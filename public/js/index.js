04979486b1784400ecda8751ae0bffcb40e8d231
Stella Jiang <stjiang@iu.edu>
2020-09-18 15:32:41 -0400

    new

--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,12 @@ var app = new Vue({
     el: '#random',
     data: {
       userName: 'John Doe',
+      userCountry: 'Canada',
+      userDOB: '1998-09-17',
+      userAge: '22',
       userEmail: 'jdoe@iu.edu',
+      userImgLarge:'',
+      userImgThumb:'',
     },
     created() {
       this.fetchUser();
@@ -17,9 +22,27 @@ var app = new Vue({
           console.log(userData);
           this.userName = userData.name.first + " " + userData.name.last;
           this.userCountry = userData.location.country
-          this.userDOB = userData.dob.date[0,9]
+          this.userDOB = userData.dob.date.slice(0,10)
           this.userAge = userData.dob.age
           this.userEmail = userData.email
+          this.userImgLarge = userData.picture.large;
+          this.userImgThumb = userData.picture.thumbnail;
         })
+        .catch((error)=>{
+          console.error('Error:',error);
+          fetch('./JSON/randomuser.me-sample.json')
+          .then(response => response.json())
+          .then(data =>  {
+            var userData =data.results[0];
+            console.log(userData);
+            this.userName = userData.name.first + " " + userData.name.last;
+            this.userCountry = userData.location.country
+            this.userDOB = userData.dob.date.slice(0,10)
+            this.userAge = userData.dob.age
+            this.userEmail = userData.email
+            this.userImgLarge = userData.picture.large;
+            this.userImgThumb = userData.picture.thumbnail;
+          })
+        });
       }}
   })
