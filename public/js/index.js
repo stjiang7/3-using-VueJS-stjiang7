4f95eacd5087040cc706849aeecd39ad255164c1
Stella Jiang <stjiang@iu.edu>
2020-09-17 23:13:37 -0400

    Save

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,25 @@
 var app = new Vue({
     el: '#random',
     data: {
-      message: 'Hello Vue!'
-      userName: ''
-    }
+      userName: 'John Doe',
+      userEmail: 'jdoe@iu.edu',
+    },
+    created() {
+      this.fetchUser();
+    },
+
+    methods: {
+      fetchUser: function() {
+        fetch('https://randomuser.me/api/')
+        .then(response => response.json())
+        .then(data =>  {
+          var userData =data.results[0];
+          console.log(userData);
+          this.userName = userData.name.first + " " + userData.name.last;
+          this.userCountry = userData.location.country
+          this.userDOB = userData.dob.date[0,9]
+          this.userAge = userData.dob.age
+          this.userEmail = userData.email
+        })
+      }}
   })
